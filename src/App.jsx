516a37c8282128de61b19ffd489b5f317d35e5d8
AmanSingh494 <amanforwork1@gmail.com>
2024-04-26 20:01:13 +0530

    created navbar for phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-// import { useState } from 'react'
-import './App.css'
+import { createGlobalStyle } from 'styled-components'
 import Navbar from './components/Navbar'
+const GlobalStyle = createGlobalStyle`
+  @media (max-width: 768px) {
+    body {
+      /* font-size: 14px; */
+    }
+  }
+`
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Navbar />
-    </div>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
 import styled from 'styled-components'
 
 const NavbarContainer = styled.div`
@@ -7,7 +5,6 @@ const NavbarContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 10px;
-  border-radius: 9px;
   box-shadow: 0px 2px 9px 3px #b3a6a6;
 `
 
@@ -19,23 +16,31 @@ const LogoContainer = styled.div`
 const LogoImage = styled.img`
   width: 50px;
   height: 50px;
+
+  /* @media (max-width: 768px) {
+    width: 40px;
+    height: 40px;
+  } */
 `
 
 const LogoName = styled.div`
-  margin-left: 10px;
+  /* margin-left: 10px; */
 `
 
 const LinksContainer = styled.div`
   display: flex;
   gap: 20px;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `
 
 const Navbar = () => {
   return (
     <NavbarContainer>
       <LogoContainer>
-        <LogoImage src='logo.png' alt='logo' />
-        <LogoName>Company Name</LogoName>
+        <LogoImage src='src/assets/img/crafted-concepts-logo.png' alt='logo' />
+        <LogoName>Crafted-Concepts</LogoName>
       </LogoContainer>
       <LinksContainer>
         <a href='#'>Link 1</a>
